4a4a1a8c9528af7b70c83d9860380fee9357f35e
Krista Calderon <kamsci@gmail.com>
2016-08-08 22:19:46 -0700

    added borrow.html state & file; added jquery, and script and css file placers for ng-animate

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-var BorrowApp = angular.module('BorrowApp', ['ui.router']);
+var BorrowApp = angular.module('BorrowApp', ['ui.router', 'ngAnimate']);
 
 BorrowApp.config(['$stateProvider', '$urlRouterProvider', 
   function($stateProvider, $urlRouterProvider) {
@@ -11,8 +11,24 @@ BorrowApp.config(['$stateProvider', '$urlRouterProvider',
       templateUrl: 'app/views/main.html',
       controller: 'MainCtrl'
     })
+    .state('stuff', {
+      url: '/borrow-stuff',
+      templateUrl: 'app/views/borrow.html',
+      controller: 'BorrowCtrl'
+    })
+
   }]) // end config
 
 BorrowApp.controller('MainCtrl', ['$scope', function($scope) {
   $scope.borrow = "BegBorrowNeverSteal";
-}])
+  // add search for users
+  $scope.allUsers = ['Amy', 'Jon', 'Travis'];
+  // function for sending user selection to borrow page
+  $scope.userSelect = function(id){
+
+  };
+}]); // end MainCtrl
+
+BorrowApp.controller('BorrowCtrl', ['$scope', function($scope) {
+  $scope.borrowStuff = " Stuff to Borrow";
+}]); // end BorrowCtrl
