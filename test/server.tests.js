c74344739e875217a42570329be1fe327ec8f8e4
Krista Calderon <kamsci@gmail.com>
2016-08-22 23:38:21 -0700

    added new test for api/borrow-stuff/:id plus 2 test frameworks for 2 new features

--- test/server.tests.js
+++ test/server.tests.js
@@ -4,21 +4,49 @@ var app = require('../index.js');
 var db = require('../models');
 //////////////////////////////////////
 
-before(function(done) {
-  db.sequelize.sync({ force: true }).then(function() {
-    done();
-  });
+//// INSTRUCTIONS ////
+// Run sequelize db:seed:all in the test database to have appropriate data for testing 
+/////////////////////
+
+describe('POST /signin - Create a user: NEW FEATURE', function(done) {
+  it('should give a 200 response');
+  it('should create a user if the email is unique and all fields complete');
+  it('should not create a user if the email is not unique');
+  it('should not create a user if a field is missing');
 });
 
-describe('Creating an item via /new-stuff', function() {
+describe('GET /login - User can login: NEW FEATURE', function(done) {
+  it('should give you a 200 response');
+  it('should verify user exits in db, log them in and redirect to borrow page');
+  it('should verify if user does not exist, they are redirected to signin');
+});
+
+describe('GET /borrow-stuff/:id - Displays items of non-currentlly-selected users only', function(done) {
+  it('should give you a 200 response', function(done) {
+    request(app).get('/api/borrow-stuff/' + 1).expect(200, done);
+  });
+  it('should return items that do not belong to the currentlly selected user(1)', function(done) {
+    request(app).get('/api/borrow-stuff/' + 1)
+    .end(function(err, res) {
+      expect(res.body).to.have.lengthOf(7);
+      for(var i = 0; i < res.body.length; i++) {
+        expect(res.body[i].userId).to.not.equal(1);  
+      }
+      done();
+    })
+  });
+
+})
+
+describe('/new-stuff - Creating an item by user', function() {
   it('should give a 200 response - /new-stuff', function(done) {
-    request(app).get("/").expect(200, done);
+    request(app).get("/api/new-stuff").expect(200, done);
   })
   it('should create a single item on /new-stuff', function(done) {
     request(app).post('/api/new-stuff')
       .type('form')
       .send({
-        userId: 1,
+        user_id: 1,
         name: 'Yarn',
         category: 'Indoor Fun',
         description: 'Rainbow yarn, medium weight, great for scarves',
@@ -29,8 +57,8 @@ describe('Creating an item via /new-stuff', function() {
         borrowerID: null
       })
       .end(function(err, res) {
-        console.log(res)
-        // expect(res.body).to.have.property('userId', 1);
+        console.log(res.body);
+        expect(res.body).to.have.property('userId', 1);
         expect(res.body).to.have.property('name', 'Yarn');
         expect(res.body).to.have.property('category', 'Indoor Fun');
         expect(res.body).to.have.property('description', 'Rainbow yarn, medium weight, great for scarves');
